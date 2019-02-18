d8e820c1d69dd4650a14504712e650723d60cc8a
aupasana <aupasana@users.noreply.github.com>
2019-02-18 15:42:39 -0800

    show synonyms in answer

--- app.py
+++ app.py
@@ -9,11 +9,19 @@ def index():
     all_vargas = ['स्वर्गवर्गः','व्योमवर्गः','दिग्वर्गः','कालवर्गः','धीवर्गः','शब्दादिवर्गः','नाट्यवर्गः','पातालभोगिवर्गः','नरकवर्गः','वारिवर्गः','भूमिवर्गः','पुरवर्गः','शैलवर्गः','वनौषधिवर्गः','सिंहादिवर्गः','मनुष्यवर्गः','ब्रह्मवर्गः','क्षत्रियवर्गः','वैश्यवर्गः','शूद्रवर्गः','विशेष्यनिघ्नवर्गः','सङ्कीर्णवर्गः','विशेष्यनिघ्नवर्गः','सङ्कीर्णवर्गः','नानार्थवर्गः','अव्ययवर्गः']
     return render_template('index.html', all_vargas=all_vargas)
 
+def getMainTokenPart(token_line):
+    line_parts = token_line.split(",")
+    return line_parts[0]
+
+def getSynonymTokenPart(token_line):
+    line_parts = token_line.split(",")
+    return line_parts[4]
+
 @app.route('/quiz')
 def quiz():
 
-    filter = request.args.get('varga')
-    filename = 'amara/tokens/tokens_' + filter + '.utf8'
+    filter_name = request.args.get('varga')
+    filename = 'amara/tokens/tokens_' + filter_name + '.utf8'
     # filename = secure_filename(filename)
     lines = open(filename).read().splitlines()
 
@@ -22,12 +30,14 @@ def quiz():
 
     question = question_line_parts[0]
     sloka_index = question_line_parts[1]
+    linga = question_line_parts[2]
     varga = question_line_parts[3]
     sloka_index_parts = sloka_index.split(".")
     sloka_index_relevant_parts = sloka_index_parts[0:3]
     sloka_index_line = sloka_index_parts[3]
     sloka_number = ".".join(sloka_index_relevant_parts)
     answer = question_line_parts[4]
+    synonym = question_line_parts[4]
     answer = answer.replace("_", " ")
 
     context_filename = 'amara/slokas/Sloka_' + sloka_number + '.utf8'
@@ -37,7 +47,13 @@ def quiz():
     context[context_index] = '<span class="highlight">' + context[context_index] + '</span>'
     context_html = "<br/>".join(context)
 
-    return render_template('quiz.html', question=question, varga=varga, answer=answer, sloka_index=sloka_index, sloka_number=sloka_number, context=context_html, filter=filter)
+    isSynonym = lambda token_line: true; # synonym == "abc"; getSynonymTokenPart(token_line);
+
+    synonyms = list(filter(lambda token_line: getSynonymTokenPart(token_line) == synonym, lines))
+    synonyms_main_part = map(getMainTokenPart, synonyms)
+    synonyms_string = " ".join(synonyms_main_part)
+
+    return render_template('quiz.html', question=question, varga=varga, answer=answer, sloka_index=sloka_index, sloka_number=sloka_number, context=context_html, filter=filter_name, synonyms=synonyms_string, linga=linga)
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
