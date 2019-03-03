bca5531ffde3447b1b87f89ba287e97de74a1272
aupasana <aupasana@users.noreply.github.com>
2019-03-02 21:35:07 -0800

    add /about

--- docker/app.py
+++ docker/app.py
@@ -190,5 +190,10 @@ def varga():
     finally:
         con.close()
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
