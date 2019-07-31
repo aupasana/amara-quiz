b66e584d6fb023dfdbb1337a4387b9cadbe8264f
aupasana <aupasana@users.noreply.github.com>
2019-07-30 17:35:05 -0700

    update build and deploy scripts for other koshas

--- app/init.sh
+++ app/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 gunzip amara.db.gz
+gunzip koshas_mulam.db.gz
 gunzip babylon.db.gz
 
 exec "$@"
--- scripts/docker_build_run.sh
+++ scripts/docker_build_run.sh
@@ -1,5 +1,7 @@
 rm app/*.db app/*.gz
 cp database/amara.db app
+cp database/koshas_mulam.db app
 gzip app/amara.db
+gzip app/koshas_mulam.db
 docker build -t aupasana/amara-quiz app
 docker run -p 8888:5000 aupasana/amara-quiz
