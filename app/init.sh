e2d22b027d4aa833f6b2d191a50d71caddaf5560
aupasana <aupasana@users.noreply.github.com>
2019-07-31 10:32:44 -0700

    deployment for shabda rupavali

--- app/init.sh
+++ app/init.sh
@@ -3,5 +3,6 @@
 gunzip amara.db.gz
 gunzip koshas_mulam.db.gz
 gunzip babylon.db.gz
+gunzip rupavali_shabda.db.gz
 
 exec "$@"
--- scripts/docker_build_run.sh
+++ scripts/docker_build_run.sh
@@ -1,7 +1,9 @@
 rm app/*.db app/*.gz
 cp database/amara.db app
 cp database/koshas_mulam.db app
+cp database/rupavali_shabda.db app
 gzip app/amara.db
 gzip app/koshas_mulam.db
+gzip app/rupavali_shabda.db
 docker build -t aupasana/amara-quiz app
 docker run -p 8888:5000 aupasana/amara-quiz
